b0121f86570b957eac72ad819cffb50fcdf608e8
Mateus Oliveira <mateusocb@gmail.com>
2016-11-05 03:38:44 -0300

    heroku funcione

--- src/main/java/br/edu/ifrn/apifyp/ApifypApplication.java
+++ src/main/java/br/edu/ifrn/apifyp/ApifypApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApifypApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApifypApplication.class, args);
-	}
+    protected ApifypApplication() {
+    }
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(ApifypApplication.class, args);
+//	}
 }
